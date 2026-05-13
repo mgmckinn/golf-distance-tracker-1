eaa9e897aab083dc7f8168e1fad24e5dd54de382
NoblCoder <mitch.g.mckinney@gmail.com>
2026-05-13 14:18:32 -0400

    Updates

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+/**
+ * eslint-disable no-restricted-globals
+ *
+ * @format
+ */
+
+// This service worker can be customized!
+// See https://developers.google.com/web/tools/workbox/modules
+// for the list of available Workbox modules, or add any other
+// code you'd like.
+// You can also remove this file if you'd prefer not to use a
+// service worker, and the Workbox build step will be skipped.
+
+const CACHE_NAME = "golf-distance-tracker-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/js/bundle.js",
+  "/static/css/main.css",
+  "/manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
+});
